efeaf9bbed01821ec5e739999f9a7c0a5aa7a32d
MarinaHurricane <maryishch88@gmail.com>
2026-09-25 00:02:32 +0100

    update user logout

--- src/controllers/auth/logoutUser.js
+++ src/controllers/auth/logoutUser.js
@@ -4,7 +4,7 @@ export const logoutUser = async (req, res) => {
   const { sessionId } = req.cookies;
 
   if (sessionId) {
-    await prisma.session.delete({
+    await prisma.session.deleteMany({
       where: {
         id: Number(sessionId),
       },
